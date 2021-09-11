4f66f34640cda23c3c6371ba2f1748c0fc654847
AyanamiKiki <ayanami.kiki@gmail.com>
2021-09-11 09:27:31 +0200

    Linked Git repo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,18 @@
 
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
     React Weather App
+    <footer> Open Source Code by:{" "}
+    < a href="https://github.com/AyanamiKiki/weather_app_react_project" 
+    target="_blank"
+    rel="noreferrer">Ayanami Kiki
+    </a>
+    </footer>
     </div>
   );
 }
 
-export default App;
+
