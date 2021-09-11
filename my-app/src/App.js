b4ba09c042e05dcedc0fc474d34f5e923f641a3f
AyanamiKiki <ayanami.kiki@gmail.com>
2021-09-11 18:21:50 +0200

    HTML added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
-
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
+
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-    <h1>React Weather App</h1>
+<Weather />
     <footer> Open Source Code by:{" "}
     < a href="https://github.com/AyanamiKiki/weather_app_react_project" 
     target="_blank"
