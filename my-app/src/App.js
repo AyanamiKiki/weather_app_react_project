209f29e48ba97f7f1842aa21afe248cb51fb7087
AyanamiKiki <ayanami.kiki@gmail.com>
2021-09-11 10:01:30 +0200

    Bootstrap added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-    React Weather App
+      <div className="container">
+    <h1>React Weather App</h1>
     <footer> Open Source Code by:{" "}
     < a href="https://github.com/AyanamiKiki/weather_app_react_project" 
     target="_blank"
     rel="noreferrer">Ayanami Kiki
     </a>
     </footer>
+    </div>
     </div>
   );
 }
